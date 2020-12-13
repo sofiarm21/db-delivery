31b05d08fedbc9b71223b8d997d9a37354cecef4
cafesaro <33528930+cafesaro@users.noreply.github.com>
2020-12-12 23:31:53 -0400

    Update 2_GENERAL_PEDIDOS.sql

--- simulaciones/principales/2_GENERAL_PEDIDOS.sql
+++ simulaciones/principales/2_GENERAL_PEDIDOS.sql
@@ -1,4 +1,4 @@
-create OR REPLACE procedure GENERAL_PEDIDOS(P integer, today in out date)
+create OR REPLACE  procedure GENERAL_PEDIDOS(P integer, today in out date)
 is
 n_pedidos integer;
 begin
@@ -10,13 +10,13 @@ loop
     /* SE LLEVA AL CABO LA SIMULAICION 7*/
     DBMS_OUTPUT.PUT_LINE('1. Se lleva al cabo la simulacion de crear pedido');
     DBMS_OUTPUT.PUT_LINE('Se crea el pedido Nro'|| n_pedidos);
-    /* EXCECUTE crear_pedidos(today)*/
+    CREAR_PEDIDOS(TODAY => today);
     TODAY:= today + 30/(24*60);
     exit when n_pedidos = p;
 end loop;
     /* SE LLEVA AL CABO LA SIMULAICION 8*/
     DBMS_OUTPUT.PUT_LINE('2. Se lleva al cabo la simulacion de asignar transportes a pedido');
-    /*execute immediate asignacion_pedidos_transportes(today);*/
+    asignacion_pedidos_transportes(TODAY => TODAY);
     /* SE LLEVA AL CABO LA SIMULAICION 9*/
     DBMS_OUTPUT.PUT_LINE('3. Se lleva al cabo la simulacion de depachar pedido');
     /*execute immediate despachar_pedidos(today);*/
