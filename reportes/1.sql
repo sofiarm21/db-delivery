ee993dd4848251e34faa825574012df8ebbbe45a
cafesaro <33528930+cafesaro@users.noreply.github.com>
2020-12-15 12:04:10 -0400

    Update 1.sql

--- reportes/1.sql
+++ reportes/1.sql
@@ -5,13 +5,14 @@ INNER JOIN acuerdos_servicios a
 ON a.id_proveedor = p.id
 ;
 
-CREATE OR REPLACE PROCEDURE reporte_1(cur OUT sys_refcursor) IS
+create or replace PROCEDURE reporte_1(cur OUT sys_refcursor) IS
 BEGIN
     OPEN cur FOR
-    SELECT p.datos_empresa.nombre AS Empresa, CONCAT(a.n_envios,CONCAT(' envíos * ', a.ocurrencia)) AS Presupuesto, CONCAT(CONCAT(a.rango_fecha.fecha_inicio, ' '), a.rango_fecha.fecha_fin) AS Vigencia, CONCAT('$ ', CONCAT(a.precio,' /mensual')) AS Precio
+    SELECT p.datos_empresa.nombre AS Empresa,p.LOGO as LOGO, CONCAT(a.n_envios,CONCAT(' * ', a.ocurrencia)) AS Presupuesto, CONCAT(CONCAT (a.rango_fecha.fecha_inicio,' - '), a.rango_fecha.fecha_fin) AS Vigencia, CONCAT('$ ', a.precio) AS Precio
     FROM proveedores  p
     INNER JOIN acuerdos_servicios a
     ON a.id_proveedor = p.id
     ;
 END;
 /
+
